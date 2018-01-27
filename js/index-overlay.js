24579087e979a6843432aa6c7bf93faf925f2e37
aishwaryamore <aishwaryamore>
2018-01-27 23:59:31 +0530

    Added collapasable navigation bar and footer

---
+++ js/index-overlay.js
@@ -0,0 +1,9 @@
+function openNavbar() {
+    document.getElementById("myNavbar").style.height = "70%";
+    document.getElementById("open").style.visibility = "hidden";
+}
+
+function closeNavbar() {
+    document.getElementById("myNavbar").style.height = "0%";
+    document.getElementById("open").style.visibility = "visible";
+}
